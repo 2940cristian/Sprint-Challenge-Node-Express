321e195ba741df31e89b30548c8a3d5b06ccd974
2940cristian <2940cristian@gmail.com>
2018-05-04 10:47:58 -0700

    Got projects to display in react app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,97 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios"
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      projects: [],
+      mounted: false,
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      mounted: true,
+    })
+    this.fetchData();
+   };
+
+
+
+   fetchData = () => {
+    axios.get("http://localhost:5000/api/projects")
+    .then(response => {
+      this.setState({
+        projects: response.data
+      })
+      console.log(this.state.projects)
+    })
+    .catch(err => {
+      console.log("There was an error fetching posts")
+    })
+  }
+
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+
+  addProject = event => {
+    event.preventDefault();
+    const newProject = {
+      name: this.state.name,
+      description: this.state.description,
+      completed: false
+    }
+
+    axios.post(("http://localhost:5000/api/projects"), newProject)
+      .then(postSend => {
+        this.fetchData();
+      })
+      .catch(err => {
+        console.log("Error sending post")
+      })
+
+      this.setState({
+        name: '',
+        description: ''
+      })
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      {this.state.mounted === false ? (
+        <div>Fetching....</div>
+      ) : (
+        <div>
+          <div className="banner">
+            Projects!
+          </div>
+          <ul>
+            {this.state.projects.map(project => {
+              return(
+                <li key={project.id} className="card">
+                  <h2>{project.name}</h2>
+                  <p>{project.description}</p>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      )}
+      <div className="form">
+        <form onSubmit={this.addProject}>
+              <input onChange={this.handleInputChange} name="name" placeholder="Name" type="text"/>
+              <input onChange={this.handleInputChange} name="description" placeholder="Description" type="text"/>
+              <button type="submit">Submit</button>
+          </form>
+      </div>
       </div>
     );
   }
